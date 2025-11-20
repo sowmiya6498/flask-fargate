17b47ec44b6735e8f3145f7812244d32fc0548a2
root <root@ip-172-31-38-97.ap-south-1.compute.internal>
2025-11-20 18:30:15 +0000

    my update

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Fargate!"
+    return "Hello from AWS Fargate on ECS via ALB!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
